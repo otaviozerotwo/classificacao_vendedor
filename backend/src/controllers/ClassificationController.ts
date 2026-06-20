a174f25f1cc8f68f19cec6551134a7eae2baf30e
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2026-06-20 11:46:42 -0300

    fix/correção caminhos imports service & controller

--- backend/src/controllers/ClassificationController.ts
+++ backend/src/controllers/ClassificationController.ts
@@ -1,10 +1,16 @@
 import type { Request, Response } from 'express'
-import { classify } from '~/services/classificationService.js'
+import { classify } from '../services/classificationService.js'
 
 export const ClassificationController = (req: Request, res: Response) => {
-  const result = classify(req.body)
-
-  return res.json({
-    result
-  })
+  try {
+    const result = classify(req.body)
+  
+    if (result) {
+      return res.json({ result })
+    } else {
+      return res.status(400).json()
+    }
+  } catch (error) {
+    
+  }
 }
--- backend/src/routes/classificationRoutes.ts
+++ backend/src/routes/classificationRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { ClassificationController } from '~/controllers/ClassificationController.js'
+import { ClassificationController } from '../controllers/ClassificationController.js'
 
 const router = Router()
 
